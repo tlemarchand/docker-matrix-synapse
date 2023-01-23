f5aa8d64a8b4c082bc85473a191a3ec62f0a6454
Thomas Lemarchand <tlemarchand@users.noreply.github.com>
2023-01-23 08:52:41 +0100

    Git commit simplified

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 git checkout master
 curl -s 'https://pypi.org/pypi/matrix-synapse/json' | jq -j -r '.info.version' > version
 git add -A
-git commit -m `cat version`
+git commit -m "Version update"
 git push origin master
 git tag `cat version`
 git push origin `cat version`
